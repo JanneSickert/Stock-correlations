12efcd6cdf8c42d5732d0334ad900229f32836dd
Janne Sickert <58070646+JanneSickert@users.noreply.github.com>
2021-06-01 19:45:37 +0200

    Delete WasTested.java

--- src/parser/WasTested.java
+++
@@ -1,9 +0,0 @@
-package parser;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target({ElementType.METHOD})
-public @interface WasTested {
-	// Methods have this Annotation if they where tested.
-}
